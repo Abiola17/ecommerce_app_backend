138ffac133aeefa8d07ea98b73de49bbf4ac2c00
Abiola17 <josephsegun696@gmail.com>
2025-02-25 12:36:18 +0100

    Ecommerce app with firebase backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:e_commerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
 
-
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then(
+        // Inject AuthenticationRepository using GetX
+      
+      );
   runApp(const App());
 }
